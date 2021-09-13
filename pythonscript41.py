54472ba8df44d418c6ecd5c709485305dab558cb
vijeandran <87280096+vijeandran@users.noreply.github.com>
2021-09-13 21:02:19 +0530

    Initial Commit

---
+++ pythonscript41.py
@@ -0,0 +1,20 @@
+'''
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+7 7 7 7 7 7 7
+8 8 8 8 8 8 8 8
+9 9 9 9 9 9 9 9 9
+'''
+
+
+x = int(input("Enter number of rows: "))
+
+for i in range(1,x+1):
+    print((str(i)+' ')*i)
+    
+    
+
